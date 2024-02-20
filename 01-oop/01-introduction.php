09aad1d0cf97eb48c9a1ab5d4aaeb8800153990b
JULI1926 <jdlondono8@misena.edu.co>
2024-02-20 18:58:42 -0500

    POO

--- 01-oop/01-introduction.php
+++ 01-oop/01-introduction.php
@@ -23,8 +23,34 @@ if(password_verify($string, $hash)){
     echo "Verified Succesfull";
 }
 
+function adition($num1, $num2) {
+    return($num1 + $num2) ; // Add a semicolon here
+}
+
+echo "<br>";
+
+$res = adition(34, 890) ;
+echo $res;
+
 # Structured Programing
 # Object oriented Programming
+
+class Adition {
+    public $num1;
+    public $num2;
+
+    public function getResult(){
+        return($this->num1 + $this->num2);
+    }
+
+}
+
+$sum = new Adition;
+$sum->num1 = 1024;
+$sum->num2 = 512;
+
+echo "<br>La Suma de {$sum->num1} y {$sum->num2} es = ". $sum->getResult();
+
 ?>
 
 
