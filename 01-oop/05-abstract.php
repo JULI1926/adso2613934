d90ddefed8d3dcd692e31e3daa7a6a30291818e7
JULI1926 <jdlondono8@misena.edu.co>
2024-03-11 20:31:14 -0500

    Se muestran pokemones en tabla

--- 01-oop/05-abstract.php
+++ 01-oop/05-abstract.php
@@ -59,56 +59,104 @@
     <nav class="controls">
         <a href="index.html">
             <svg xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-            <path fill="#ffffff"
-            d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM231 127c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-71 71L376 232c13.3 0 24 10.7 24 24s-10.7 24-24 24l-182.1 0 71 71c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L119 273c-9.4-9.4-9.4-24.6 0-33.9L231 127z" />
-        </svg>
-    </a>
-</nav>
-<main>
-    <h1>05 - Abstract</h1>
-    <section>
-        <?php
-            abstract class Database {
+                viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                <path fill="#ffffff"
+                    d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM231 127c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-71 71L376 232c13.3 0 24 10.7 24 24s-10.7 24-24 24l-182.1 0 71 71c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L119 273c-9.4-9.4-9.4-24.6 0-33.9L231 127z" />
+            </svg>
+        </a>
+    </nav>
+    <main>
+        <h1>05 - Abstract</h1>
+        <section>
+            <?php
+            abstract class Database
+            {
                 // Atributos
                 protected $host;
                 protected $user;
                 protected $pass;
                 protected $dbname;
                 protected $conx;
-            
+
                 // Método constructor
-                public function __construct($dbname, $host='localhost', $user='root', $pass='') {
+                public function __construct($dbname, $host = 'localhost', $user = 'root', $pass = '')
+                {
                     $this->host = $host;
                     $this->user = $user;
                     $this->pass = $pass;
                     $this->dbname = $dbname;
 
                 }
-            
+
                 // Método para conectarse a la base de datos
-                public function connect() {
-                    try {                        
+                public function connect()
+                {
+                    try {
                         $this->conx = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->user, $this->pass);
-                        echo "Conexion establecida";                    
+                        echo "Conexion establecida";
                     } catch (PDOException $e) {
                         echo $e->getMessage();
                     }
-                }    
-            }
-            
-            class Pokemon extends Database {
+                }
+
 
             }
-            
+
+            class Pokemon extends Database
+            {
+                // Método para listar pokemones
+                public function listPokemons()
+                {
+                    try {
+                        $query = "SELECT * FROM pokemons";
+                        $statement = $this->conx->prepare($query);
+                        $statement->execute();
+                        return $statement->fetchAll(PDO::FETCH_ASSOC);
+                    } catch (PDOException $e) {
+                        echo $e->getMessage();
+                        return false;
+                    }
+                }
+            }
+
+
+
             // Crear una instancia de Pokemon
-            $db = new Pokemon('adsodd2613934');
+            $db = new Pokemon('adso2613934');
             $db->connect();
-            
+            $pokemons = $db->listPokemons();
 
-            
-        ?>
-    </section>
+
+
+            ?>
+        </section>
+        <section class="pokemon-container">
+            <table class="pokemon-table">
+                <tr>
+                    <th>Name</th>
+                    <th>Type</th>
+                    <th>Health</th>
+                    <th>Image</th>
+                </tr>
+                <?php foreach ($pokemons as $pokemon): ?>
+                    <tr>
+                        <td>
+                            <?php echo $pokemon['name']; ?>
+                        </td>
+                        <td>
+                            <?php echo $pokemon['type']; ?>
+                        </td>
+                        <td>
+                            <?php echo $pokemon['health']; ?>
+                        </td>
+                        <td><img src="<?php echo $pokemon['image']; ?>" alt="<?php echo $pokemon['name']; ?>"></td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+
+        </section>
+
+
 </body>
 
 </html>
