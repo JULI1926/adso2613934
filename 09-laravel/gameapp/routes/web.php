fcb257787cc9c5451011c0e49a4f0ab6a1d4b930
JULI1926 <jdlondono8@misena.edu.co>
2024-06-21 21:36:52 -0500

    Se agrega breeze y se implementa register funcionando

--- 09-laravel/gameapp/routes/web.php
+++ 09-laravel/gameapp/routes/web.php
@@ -1,60 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Carbon\Carbon;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/games/list', function () {       
-    $games = App\Models\Game::all();
-    //echo var_dump($games);
-    dd($games->toArray());
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
-Route::get('/game/{id}', function () {       
-    $game = App\Models\Game::find(request()->id);
-    dd($game->toArray());
-});
-
-
-Route::get('/game/{id}', function () {       
-    $game = App\Models\Game::find(request()->id);
-    dd($game->toArray());
-});
-
-Route::get('/users', function () {
-    $users = User::latest()->with('birthdate')->limit(20)->get();
-
-    if (view()->exists('users.index')) {
-        return View::make('users.index', compact('users', 'currentDate'));
-    } else {
-        $usersWithAge = [];
-        $currentDate = Carbon::now();
-
-        foreach ($users as $user) {
-            $age = null;  
-
-            
-            if ($user->birthdate) {
-                $age = $currentDate->diffInYears($user->birthdate);
-            }
-
-            $usersWithAge[] = [
-                'name' => $user->name,
-                'email' => $user->email,
-                'birthdate' => $user->birthdate,
-                'age' => $age,
-            ];
-        }
-
-        return $usersWithAge;
-    }
-});
-
-Route::get('/games', function () {       
-    $games = App\Models\Game::all();
-    return view('listgames')->with('games', $games);
-});
+require __DIR__.'/auth.php';
